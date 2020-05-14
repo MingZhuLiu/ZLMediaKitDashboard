f88732c762563a1c1292092d4c12ac06132a78c1
刘明珠 <starry_link@foxmail.com>
2020-05-14 12:38:08 +0800

    初始化
    初始化

--- ZLMediaKitService/Models/Config/DatabaseConfig.cs
+++
@@ -1,8 +0,0 @@
-namespace ZLMediaKitService.Model.Config
-{
-    public class DatabaseConfig
-    {
-        public string DataBaseType{get;set;}
-        public string ConnectStr{get;set;}
-    }
-}
